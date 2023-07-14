f29bd72158bf9fa721d6e4ebbb875037875c0854
OgTochi <ogbonnatochukwu38@gmail.com>
2023-07-14 14:55:41 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/**
+ * main - main funtion
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a ; i <= '2' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
